9c3412cca9b28531eddb5ec9e900a03e1d1caa2c
AJTFATI <TAYLORALEXANDERJORDAN@GMAIL.COM>
2024-08-03 12:16:31 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'alexanderpraec',
   github: 'ajtfati',
-  linkedin: 'fisch2',
+  linkedin: 'alexanderjordantaylor',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
